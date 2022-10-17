9fd2020de2ab312ed431815de3a06f21b8798c33
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-10-18 00:10:00 +0200

    def

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $post=Post::all();
+        $post=Post::with(['category','tags'])->get();
         return response()->json([
             'success'=> true,
             'results'=> $post
--- resources/js/components/MyMain.vue
+++ resources/js/components/MyMain.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="container">
+        <div class="row">
+            <h2 class="mb-5">Posts List:</h2>
+            <div class="card col-12 mb-5" v-for="(post, index) in posts" :key="index">
+                <div class="card-body">
+                    <h5 class="card-title">{{post.title}}</h5>
+                    <p class="card-text">{{post.content}}</p>
+                    <p class="card-text">{{post.category?post.category.name:'-'}}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
 
+        </div>
     </div>
      
  </template>
@@ -9,10 +20,15 @@
  
      export default {
          name: 'MyMain',
+         data () {
+            return {
+                posts: []
+            }
+         },
          methods: {
             getPosts() {
                axios,get('/api/posts').then((response)=>{
-                console.log(response)
+                this.posts= response.data.results;
                });
             }
          },
